469ac68f96e86edf0ad92dce3b96babff3ebda3a
pufferbommy <pufferbommy@gmail.com>
2025-02-13 21:15:15 +0700

    fix: db env

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(
-  "mongodb+srv://pufferbommy:iv8yiTPyvo8U7XeH@cluster0.2uxuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-);
+const client = new MongoClient(process.env.MONGODB_URI as string);
 export const auth = betterAuth({
   database: mongodbAdapter(client.db()),
   emailAndPassword: {
